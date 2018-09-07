ce3e4b2cd057a2b25169d1937c7c541fdd8654d8
Dakota-Dunn <dakotadunn84@gmail.com>
2018-09-06 22:27:45 -0700

    Display board and player

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
+    // x pos
+    // y pos
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // If enemy is not passed boundary
+            // Move forward
+            // Increment x by speed * dt
+        // else
+            // Reset pos to start
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,7 +33,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Hero {
+    constructor () {
+        this.x = 0;
+        this.y = 0;
+        this.sprite = 'images/char-boy.png';
+    }
 
+        // Draw player sprite on current x and y coord position
+        render () {
+            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        }
+}
+        // Handle keyboard input
+            // Update player's x and y property according to input
+        // Reset Hero
+            // Set x and y to starting x and y
+
+// New Hero object
+const player = new Hero();
+// Init allEnemies array
+// For each enemy create and push new Enemy object into above array
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
