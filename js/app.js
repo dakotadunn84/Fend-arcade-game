4ce441e6e7e6013f3fba686fda5cbdfbe3a1fdbd
Dakota-Dunn <dakotadunn84@gmail.com>
2018-09-08 02:53:25 -0700

    Player movement added

--- js/app.js
+++ js/app.js
@@ -35,21 +35,51 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 class Hero {
     constructor () {
-        this.x = 0;
-        this.y = 0;
         this.sprite = 'images/char-boy.png';
+        this.step = 101;
+        this.jump = 83;
+        this.startX = this.step * 2;
+        this.startY = (this.jump * 5) - 20;
+        this.x = this.startX;
+        this.y = this.startY;
     }
 
         // Draw player sprite on current x and y coord position
         render () {
             ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
         }
-}
-        // Handle keyboard input
-            // Update player's x and y property according to input
-        // Reset Hero
-            // Set x and y to starting x and y
 
+        // Handle keyboard input
+            /** Update player's x and y property according to input
+            *
+            *@param {string} input - Direction to travel
+            *
+            */
+            handleInput(input) {
+                switch(input) {
+                    case 'left':
+                        if ( this.x > 0) {
+                            this.x -= this.step;
+                        }
+                        break;
+                    case 'up':
+                        if (this.y > this.jump) {
+                            this.y -= this.jump;
+                        }
+                        break;
+                    case 'right':
+                        if (this.x < this.step * 4) {
+                            this.x += this.step;
+                        }
+                        break;
+                    case 'down':
+                        if (this.y < this.jump * 4) {
+                            this.y += this.jump;
+                        }
+                        break;
+                }
+            }
+}
 // New Hero object
 const player = new Hero();
 // Init allEnemies array
